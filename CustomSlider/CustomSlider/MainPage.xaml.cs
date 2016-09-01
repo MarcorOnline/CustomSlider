5d71d923a116b3b2e3b0fb899d0da22a8ec5d757
Marco Rinaldi <m.rinaldi@live.com>
2016-09-01 13:56:04 +0200

    RangeSlider control

--- CustomSlider/CustomSlider/MainPage.xaml.cs
+++ CustomSlider/CustomSlider/MainPage.xaml.cs
@@ -22,47 +22,11 @@ namespace CustomSlider
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        int Min = 1;
-        int Max = 45;
-        int Size = 100;
-        int Range = 150;
-        int left = 0;
-        int right = 0;
-        Point leftPoint = new Point();
-        Point rightPoint = new Point();
-
-        Point initalPoint = new Point(0,0);
-
-        int thumb1Pos = 0;
-        int thumb2Pos = 0;
-
-
         public MainPage()
         {
             this.InitializeComponent();
             
             this.NavigationCacheMode = NavigationCacheMode.Required;
-
-            var pos = SetPosition(Thumb1Position);
-
-            Thumb1Position = 1;
-            Thumb2Position = 45;
-
-            CompositeTransform ct = new CompositeTransform();
-            ct.TranslateX = pos;
-            LeftHandle.RenderTransform = ct;
-            leftPoint.X = pos;
-            LeftHandleText.Text = Text(pos);
-
-            pos = SetPosition(Thumb2Position);
-
-            CompositeTransform rCt = new CompositeTransform();
-            rCt.TranslateX = pos;
-            RightHandle.RenderTransform = rCt;
-            rightPoint.X = pos;
-            RightHandleText.Text = Text(pos);
-
-            FillTrackGrid.Width  = FillTrack(rightPoint, leftPoint);
         }
 
         /// <summary>
@@ -80,106 +44,5 @@ namespace CustomSlider
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
         }
-        
-        //Changes left thumb
-        private void LeftHandle_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
-        {
-            var t = (sender as Grid).RenderTransform as CompositeTransform;
-            var x = (RightHandle.RenderTransform as CompositeTransform).TranslateX;
-            var f = -this.Range;
-            var c = x - this.Size * .1;
-            double translateVal = Translate(t, e, f, c);
-            t.TranslateX = Translate(t, e, f, c);
-            LeftHandleText.Text = Text(t.TranslateX);
-
-            CompositeTransform ct = new CompositeTransform();
-            ct.TranslateX = Translate(t, e, f, c);
-            FillTrackGrid.RenderTransform = ct;
-            
-            left = Convert.ToInt32(translateVal);
-
-            leftPoint.X = t.TranslateX + e.Delta.Translation.X;
-            leftPoint.Y = t.TranslateY + e.Delta.Translation.Y;
-            
-            FillTrackGrid.Width = FillTrack(rightPoint, leftPoint);
-        }
-
-        //Changes right thumb
-        private void RightHandle_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
-        {
-            var t = (sender as Grid).RenderTransform as CompositeTransform;
-            var x = (LeftHandle.RenderTransform as CompositeTransform).TranslateX;
-            var f = x + this.Size * .1;
-            var c = this.Range;
-            t.TranslateX = Translate(t, e, f, c);
-            RightHandleText.Text = Text(t.TranslateX);
-
-            double translateVal = Translate(t, e, f, c);
-            right = Convert.ToInt32(translateVal);
-
-            rightPoint.X = t.TranslateX + e.Delta.Translation.X;
-            rightPoint.Y = t.TranslateY + e.Delta.Translation.Y;
-
-            FillTrackGrid.Width = FillTrack(rightPoint, leftPoint);
-
-        }
-
-        private double Translate(CompositeTransform s, ManipulationDeltaRoutedEventArgs e, double floor, double ceiling)
-        {
-            var target = s.TranslateX + e.Delta.Translation.X;           
-
-            if (target < floor)
-                return floor;
-            if (target > ceiling)
-                return ceiling;
-            return target;
-        }
-
-        private string Text(double x)
-        {
-            var p = (x - (-this.Range)) / ((this.Range) - (-this.Range)) * 100d;
-            var v = (this.Max - this.Min) * p / 100d + this.Min;
-            return ((int)v).ToString();
-        }
-
-        public double SetPosition(int value) 
-        {
-            var p = ((value - this.Min) * 100d)/ (this.Max - this.Min);
-
-            var x = (p * (this.Range - (-this.Range)) / 100d) + (-this.Range);
-
-            return x;
-        }
-
-        public double FillTrack(Point A, Point B)
-        {
-
-            double a = A.X - B.X;
-            double b = A.Y - B.Y;
-            double distance = Math.Sqrt(a * a + b * b);
-            
-            return distance;
-
-        }
-
-        public int Thumb1Position
-        {
-            get { return thumb1Pos; }
-            set
-            {
-                thumb1Pos = value;
-            }
-        }
-
-        public int Thumb2Position
-        {
-            get { return thumb2Pos; }
-            set
-            {
-                thumb2Pos = value;
-            }
-        }
-        
     }
 }
-
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# CustomSlider
-Range control slider with two thumbs
+# RangeSlider
+Range slider control with two thumbs for Windows Runtime applications:
+
+- WinRT
+- Windows Phone 8.1 RT
+- Windows 8
+- Windows 8.1
+- Windows 10 UWP (Windows 10 and Windows 10 Mobile)
+
+[Preview](RangeSliderWindowsPreview.png)
